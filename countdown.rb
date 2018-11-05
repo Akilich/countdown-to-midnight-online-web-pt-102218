70818aff7ffd65f1b3ca8270601280705829a84a
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-05 08:18:18 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,8 @@
-#write your code here
-
-def countdown
+countdown = 10
+def countdown(#{countdown_output)
+while countdown > 0 
+puts "#{countdown} SECOND(S)!" 
+countdown -= 1
+end
+puts "HAPPY NEW YEAR!"
 end
